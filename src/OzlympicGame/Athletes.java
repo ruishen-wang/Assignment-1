326b9f53737dbcebb9579dd82c2d9c30ce9a8b14
rmit-s3585960-tao-liu <s3585960@student.rmit.edu.au>
2017-03-30 15:03:04 +1100

    Add files via upload

---
+++ src/OzlympicGame/Athletes.java
@@ -0,0 +1,13 @@
+package OzlympicGame;
+
+public class Athletes {
+  private String ID;
+  private int age;
+  private String state;
+  private int record;
+  private int points;
+  
+  public complete (game){
+	  
+  }
+}
